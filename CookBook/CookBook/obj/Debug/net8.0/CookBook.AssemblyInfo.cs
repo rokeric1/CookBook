ccaf26182a7777e8da1350a6d0f88bf4966a02bf
Umihana Šečunović <usecunovic1@etf.unsa.ba>
2024-11-03 23:45:23 +0100

    Izuzeci,pretraživanje po imenu...

--- CookBook/CookBook/obj/Debug/net8.0/CookBook.AssemblyInfo.cs
+++ CookBook/CookBook/obj/Debug/net8.0/CookBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0620cab9a67866ec9cb246f8924ff76d554c8416")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
